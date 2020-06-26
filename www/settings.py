34595d8b5ae54f9c40424da5139d29ed8e61f8b7
valik <valik@iMac-Valik.local>
2020-06-26 10:56:04 +0300

    Secret corrected

--- www/settings.py
+++ www/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '*a5cpzdgc##0fn$8&#a&1f&t^$da*rsobd3kmny@@p%(*exnn!'
 
-SECRET_KEY=os.environ.get('SECRERT_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
