e9a4915b53c3bf93374f17fbdfe451fd5ede898e
valik <valik@iMac-Valik.local>
2020-07-14 11:20:23 +0300

    Shceduler

--- cronjob.py
+++ cronjob.py
@@ -13,6 +13,6 @@ from main import cronjob
 
 # Create an instance of scheduler and add function.
 scheduler = BlockingScheduler()
-scheduler.add_job(cronjob, 'date', run_date='2020-13-07 10:49:17')
+scheduler.add_job(cronjob, 'date', run_date=datetime(2020, 7, 14, 11, 25, 17))
 
 scheduler.start()
