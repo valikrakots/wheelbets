e9ec3fad90d3e427e4213b13c6cf45fcf9eb4d97
valik <valik@iMac-Valik.local>
2020-07-16 17:49:37 +0300

    Google

--- main.py
+++ main.py
@@ -17,6 +17,42 @@ def get_html(url, params=None):
 
 
 def cronjob():
+  one = 0
+  two = 0
+  three = 0
+  four = 0
+  five = 0
+  six = 0
+  seven = 0
+  eight = 0
+  nine = 0
+  ten = 0
+  eleven = 0
+  twelve = 0
+  thirteen = 0
+  fourteen = 0
+  fiveteen = 0
+  sixteen = 0
+  seventeen = 0
+  eighteen = 0
+  one_kolvo = 0
+  two_kolvo = 0
+  three_kolvo = 0
+  four_kolvo = 0
+  five_kolvo = 0
+  six_kolvo = 0
+  seven_kolvo = 0
+  eight_kolvo = 0
+  nine_kolvo = 0
+  ten_kolvo = 0
+  eleven_kolvo = 0
+  twelve_kolvo = 0
+  thirteen_kolvo = 0
+  fourteen_kolvo = 0
+  fiveteen_kolvo = 0
+  sixteen_kolvo = 0
+  seventeen_kolvo = 0
+  eighteen_kolvo = 0
   red_kolvo = 0
   black_kolvo = 0
   grey_kolvo = 0
@@ -48,6 +84,33 @@ def cronjob():
         grey = 0
         cup += 1
         grey_kolvo += 1
+        one += 1
+        two += 1
+        three += 1
+        four += 1
+        five += 1
+        six += 1
+        seven += 1
+        eight += 1
+        nine += 1
+        ten += 1
+        eleven += 1
+        twelve += 1
+        thirteen += 1
+        fourteen += 1
+        fiveteen += 1
+        sixteen += 1
+        seventeen += 1
+        eighteen += 1
+        if ch == '2':
+          two = 0
+          two_kolvo += 1
+        elif ch == '8':
+          eight = 0
+          eight_kolvo += 1
+        elif ch == '5':
+          five = 0
+          five_kolvo += 1
         if grey_kolvo > maxi:
           maxi = grey_kolvo
         chislo = int(ch)
@@ -70,6 +133,33 @@ def cronjob():
         black += 1
         cup += 1
         red_kolvo += 1
+        one += 1
+        two += 1
+        three += 1
+        four += 1
+        five += 1
+        six += 1
+        seven += 1
+        eight += 1
+        nine += 1
+        ten += 1
+        eleven += 1
+        twelve += 1
+        thirteen += 1
+        fourteen += 1
+        fiveteen += 1
+        sixteen += 1
+        seventeen += 1
+        eighteen += 1
+        if ch == '3':
+          three = 0
+          three_kolvo += 1
+        elif ch == '9':
+          nine = 0
+          nine_kolvo += 1
+        elif ch == '6':
+          six = 0
+          six_kolvo += 1
         if red_kolvo > maxi:
           maxi = red_kolvo
         chislo = int(ch)
@@ -92,6 +182,33 @@ def cronjob():
         black = 0
         cup += 1
         black_kolvo += 1
+        one += 1
+        two += 1
+        three += 1
+        four += 1
+        five += 1
+        six += 1
+        seven += 1
+        eight += 1
+        nine += 1
+        ten += 1
+        eleven += 1
+        twelve += 1
+        thirteen += 1
+        fourteen += 1
+        fiveteen += 1
+        sixteen += 1
+        seventeen += 1
+        eighteen += 1
+        if ch == '1':
+          one = 0
+          one_kolvo += 1
+        elif ch == '4':
+          four = 0
+          four_kolvo += 1
+        elif ch == '7':
+          seven = 0
+          seven_kolvo += 1
         if black_kolvo > maxi:
           maxi = black_kolvo
         chislo = int(ch)
@@ -114,6 +231,24 @@ def cronjob():
         black += 1
         cup_kolvo += 1
         cup = 0
+        one += 1
+        two += 1
+        three += 1
+        four += 1
+        five += 1
+        six += 1
+        seven += 1
+        eight += 1
+        nine += 1
+        ten += 1
+        eleven += 1
+        twelve += 1
+        thirteen += 1
+        fourteen += 1
+        fiveteen += 1
+        sixteen += 1
+        seventeen += 1
+        eighteen += 1
         if cup_kolvo > maxi:
           maxi = cup_kolvo
         chislo = int(ch)
@@ -136,6 +271,33 @@ def cronjob():
         black += 1
         cup += 1
         red_kolvo += 1
+        one += 1
+        two += 1
+        three += 1
+        four += 1
+        five += 1
+        six += 1
+        seven += 1
+        eight += 1
+        nine += 1
+        ten += 1
+        eleven += 1
+        twelve += 1
+        thirteen += 1
+        fourteen += 1
+        fiveteen += 1
+        sixteen += 1
+        seventeen += 1
+        eighteen += 1
+        if ch == '12':
+          twelve = 0
+          twelve_kolvo += 1
+        elif ch == '18':
+          eighteen = 0
+          eighteen_kolvo += 1
+        elif ch == '15':
+          fiveteen = 0
+          fiveteen_kolvo += 1
         if red_kolvo > maxi:
           maxi = red_kolvo
         chislo = int(ch)
@@ -158,6 +320,33 @@ def cronjob():
         grey += 1
         cup += 1
         black_kolvo += 1
+        one += 1
+        two += 1
+        three += 1
+        four += 1
+        five += 1
+        six += 1
+        seven += 1
+        eight += 1
+        nine += 1
+        ten += 1
+        eleven += 1
+        twelve += 1
+        thirteen += 1
+        fourteen += 1
+        fiveteen += 1
+        sixteen += 1
+        seventeen += 1
+        eighteen += 1
+        if ch == '10':
+          ten = 0
+          ten_kolvo += 1
+        elif ch == '13':
+          thirteen = 0
+          thirteen_kolvo += 1
+        elif ch == '16':
+          sixteen = 0
+          sixteen_kolvo += 1
         if black_kolvo > maxi:
           maxi = black_kolvo
         chislo = int(ch)
@@ -180,6 +369,33 @@ def cronjob():
         black += 1
         cup += 1
         grey_kolvo += 1
+        one += 1
+        two += 1
+        three += 1
+        four += 1
+        five += 1
+        six += 1
+        seven += 1
+        eight += 1
+        nine += 1
+        ten += 1
+        eleven += 1
+        twelve += 1
+        thirteen += 1
+        fourteen += 1
+        fiveteen += 1
+        sixteen += 1
+        seventeen += 1
+        eighteen += 1
+        if ch == '11':
+          eleven = 0
+          eleven_kolvo += 1
+        elif ch == '14':
+          fourteen = 0
+          fourteen_kolvo += 1
+        elif ch == '17':
+          seventeen = 0
+          seventeen_kolvo += 1
         if grey_kolvo > maxi:
           maxi = grey_kolvo
         chislo = int(ch)
