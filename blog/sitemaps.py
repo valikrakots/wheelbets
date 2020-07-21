cab01eb4f18ba0c39f7a0803274c3eb8b5580be2
valik <valik@iMac-Valik.local>
2020-07-21 13:25:29 +0300

    yufdaiyfgaekl

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -5,7 +5,7 @@ from django.shortcuts import reverse
 class StaticViewSitemap(Sitemap):
 
     def items(self):
-        return ['blog-about', 'blog-help', 'blog-where', 'blog-home', 'blog-contacts']
+        return ['blog:blog-about', 'blog:blog-help', 'blog:blog-where', 'blog:blog-home', 'blog:blog-contacts']
 
     def location(self, item):
         return reverse(item)
