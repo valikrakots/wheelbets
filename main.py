3b1aa1aa17fe7929e06e337ee7abaf03d0630a0c
valik <valik@iMac-Valik.local>
2020-11-14 23:36:20 +0300

    vhwjf

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def cronjob():
   while(True):
     d2 = datetime.datetime.now().date()
     d3 = datetime.datetime.now()
-    if (d3.minute == 36 or d3.minute == 34) and do == 2 and d3.second == 20:
+    if (d3.minute == 42 or d3.minute == 44) and do == 2 and d3.second == 20:
       do = 1
       chrome_options = webdriver.ChromeOptions()
       chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -77,8 +77,9 @@ def cronjob():
       driver.quit()
       image = face_recognition.load_image_file("foo.jpg")
       location = face_recognition.face_locations(image)
-      encoding = face_recognition.face_encodings(
-          image.repeat(3, 2), location)[0]
+      encodings = face_recognition.face_encodings(
+          image.repeat(3, 2), location)
+      encoding = encodings[0]
       #encoding = face_recognition.face_encodings(image)[0]
       results = face_recognition.compare_faces(known_faces, encoding, 0.6)
       if True in results:
