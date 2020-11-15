a7cb919019b03b2eddf70055e3fca351a29b1937
valik <valik@iMac-Valik.local>
2020-11-15 15:55:27 +0300

    vhwjf

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def cronjob():
   while(True):
     d2 = datetime.datetime.now().date()
     d3 = datetime.datetime.now()
-    if (d3.minute == 50 or d3.minute == 52) and do == 2 and d3.second == 20:
+    if (d3.minute == 4 or d3.minute == 2) and do == 2 and d3.second == 20:
       do = 1
       chrome_options = webdriver.ChromeOptions()
       chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
