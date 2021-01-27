84720df9b9115acd0c990ab68958e6fa3f0da279
valik <valik@iMac-Valik.local>
2021-01-27 21:27:32 +0300

    Hi there

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Table
 from .models import TableImage
+from .models import Image
 
 
 admin.site.register(Table)
 admin.site.register(TableImage)
+admin.site.register(Image)
