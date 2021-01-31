0422c5c6f5ac2f7b8b9ea6efe011587dfe80e8b1
valik <valik@iMac-Valik.local>
2021-01-31 13:42:28 +0300

    Hi there

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def cronjob():
   imgjpg = Image.open("poo.jpg")
   # table1 = Imager(im=encoded)
   # table1.save()
-  faces = face_cascade.detectMultiScale(gray, 1.1, 9)
+  faces = face_cascade.detectMultiScale(gray, 1.1, 11)
   if len(faces) == 0:
     print('(My Error) There are 0 faces.\n')
     table1 = MyErrors(time=timezone.now(), ime="none")
@@ -183,7 +183,7 @@ def cronjob():
       driver.quit()
       img1 = cv2.imread('poo.jpg')
       gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-      faces = face_cascade.detectMultiScale(gray, 1.1, 9)
+      faces = face_cascade.detectMultiScale(gray, 1.1, 11)
       imgjpg = Image.open('poo.jpg')
       if len(faces) == 0:
         print('(My Error) There are 0 faces.\n')
